7396a2c00cf4cdd3e946f3beda05b3da284f0411
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2019-07-04 14:17:08 -0300

    fix: listagem de arquivos trysafe

--- src/Routes/RegisterRoutes.ts
+++ src/Routes/RegisterRoutes.ts
@@ -1,20 +1,26 @@
 import { Server } from '@hapi/hapi';
 import klawSync from 'klaw-sync';
 import path from 'path';
-import R  from 'ramda';
+import R from 'ramda';
 import { BasePath } from '../Helpers';
 
-const getFiles = (root: string) =>
-  klawSync(root, {nodir: true}).map(item => ({
-    ...item,
-    filename: path.basename(item.path)
-  }));
+const getFiles = (root: string) => {
+  try {
+    const files = klawSync(root, { nodir: true }).map(item => ({
+      ...item,
+      filename: path.basename(item.path)
+    }));
+    return files;
+  } catch {
+    return [];
+  }
+};
 
 const RegisterRoutes = async (server: Server) => {
   const rootApi = BasePath.get('api');
   const rootApp = BasePath.get('app');
 
-  const allFiles: any = R.flatten([getFiles(rootApi),getFiles(rootApp)]);
+  const allFiles: any = R.flatten([getFiles(rootApi), getFiles(rootApp)]);
 
   const files = allFiles.filter(
     item =>
@@ -25,7 +31,10 @@ const RegisterRoutes = async (server: Server) => {
 
   files.forEach(file => {
     try {
-      const version = file.path.replace(rootApi, '').replace(rootApp, '').split('/')[1];
+      const version = file.path
+        .replace(rootApi, '')
+        .replace(rootApp, '')
+        .split('/')[1];
       const controller = require(file.path).default;
       return new controller(server, version);
     } catch (err) {
